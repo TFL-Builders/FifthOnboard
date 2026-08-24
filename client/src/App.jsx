cbd0fa1ff00a94d43aeb55976f61a68e935d116b
MusaYashim <musanatha39@gmail.com>
2026-08-24 22:00:16 +0100

    reset password page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 // import Login from './Pages/Login'
 // import ForgotPass from './Pages/ForgotPass'
 import CreateAcc from './Pages/CreateAcc'
+import ResetPass from './Pages/ResetPass'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<CreateAcc />} />
+        <Route path="/login" element={<ResetPass />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
