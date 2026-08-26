6bc0281772d9bbeb591347baaa339995252f4b43
Anyanwuocha-Collins David Chukwuemeka <anyanwuochacollinsdavid@gmail.com>
2026-08-26 11:33:25 +0100

    changes

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -58,7 +58,7 @@ const [showPassword, setShowPassword] = useState(false);
                             </div>
                 </div>
             </div>
-            <div className="text-[#64748B] c2026">© 2026 Contract Management Platform. All rights reserved.</div>
+            <div className="text-[#64748B] c2026">© 2026 Staff Onboarding Platform. All rights reserved.</div>
         </div>
     </div>
   )
